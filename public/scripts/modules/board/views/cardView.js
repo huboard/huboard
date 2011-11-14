87273223afaf676aa9a0eed7b690a0eccdfe5cfe
Ryan Rauh <rauh.ryan@gmail.com>
2011-11-13 22:07:30 -0600

    more view clean up

--- public/scripts/modules/board/views/cardView.js
+++ public/scripts/modules/board/views/cardView.js
@@ -3,13 +3,12 @@ define(["text!../templates/card.tmpl"],function(template){
   return Backbone.View.extend({
      initialize: function ( params ) {
        this.issue = params.issue;
-       this.render();
      },
      events: {
       "click h3" : "clicked"
      },
      render: function(){
-       this.el =  _.template(template, this.issue);
+       $(this.el).html( _.template(template, this.issue));
        this.delegateEvents();
        return this;
      },
--- public/scripts/modules/board/views/columnView.js
+++ public/scripts/modules/board/views/columnView.js
@@ -3,20 +3,26 @@ define(["text!../templates/column.tmpl","./cardView"],function(template, CardVie
   var Column = Backbone.View.extend({
     initialize : function(params) {
       this.column = params.column;
-      this.render();
     },
     render: function(){
       var column = $(_.template(template, this.column));
 
       _.each(this.column.issues, function(issue){
         var card = new CardView({issue : issue});
-        column.append(card.el);
+        column.append(card.render().el);
         card.delegateEvents();
       });
 
       this.el = column;
 
       return this;
+    },
+    events: {
+      "click h3" : "clicked"
+    },
+    clicked: function(){
+
+      console.log("column:click");
     }
   });
   return Column;
