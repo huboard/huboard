0184e5941795a827519617f9418e6dc3028bb146
Ryan Rauh <rauh.ryan@gmail.com>
2011-12-16 17:39:44 -0600

    added clear milestones feature

--- public/scripts/modules/board/views/sidebarView.js
+++ public/scripts/modules/board/views/sidebarView.js
@@ -1,24 +1,37 @@
 define(["../events/postal"], function (postal) {
 
   return Backbone.View.extend( { 
-     tagName: "ul",
-     className: "filters",
-     initialize: function (params) {
-         this.milestones = params.milestones;
-     },
-     render: function () {
-       var $this = $(this.el);
-       _.each(this.milestones, function (milestone) {
-         var filter = $(_.template("<li class='drop-shadow'><a href='#'><strong><%= open_issues %></strong><%= title %></a></li>", milestone))
-            .click(function(){
-              postal.publish("Filter.Milestone", function (issue) { return issue.milestone ? issue.milestone.number === milestone.number : false; });
-            })
-            .appendTo($this);
-          postal.subscribe("Filter.Milestone", function (equal) {
-             equal({milestone:milestone}) ? filter.addClass("state-active") : filter.removeClass("state-active");
-          });
-       });
-       return this;
-     }
+    tagName: "ul",
+    className: "filters",
+    initialize: function (params) {
+      this.milestones = params.milestones;
+    },
+    render: function () {
+      var $this = $(this.el),
+          clear = $("<li class='hide clear-filters'><a href='#'>Clear active filters</a></li>");
+
+      clear.click(function(ev){
+         ev.preventDefault();
+         postal.publish("Filter.Milestone", function (issue,override) { return (override != null) ? override : true; });
+         clear.hide();
+      });
+
+
+      clear.appendTo($this);
+
+      _.each(this.milestones, function (milestone) {
+        var filter = $(_.template("<li class='drop-shadow'><a href='#'><strong><%= open_issues %></strong><%= title %></a></li>", milestone))
+        .click(function(){
+          postal.publish("Filter.Milestone", function (issue) { return issue.milestone ? issue.milestone.number === milestone.number : false; });
+        })
+        .appendTo($this);
+        postal.subscribe("Filter.Milestone", function (equal) {
+          var active = equal({milestone:milestone},false);
+          active ? filter.addClass("state-active") : filter.removeClass("state-active");
+          if (active) {clear.show();}
+        });
+      });
+      return this;
+    }
   });
 });
