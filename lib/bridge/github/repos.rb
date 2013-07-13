9737479d30146f1e78c9a79ad9dffaf7f4974d4c
Ryan Rauh <rauh.ryan@gmail.com>
2013-07-12 22:34:22 -0500

    handle error where github api fails to return repos

--- lib/bridge/github/repos.rb
+++ lib/bridge/github/repos.rb
@@ -22,7 +22,7 @@ class Huboard
       user = connection.users user
       the_repos = repos(user.login) if user.type == "Organization"
       the_repos = user.repos.all.sort_by{|r| r["pushed_at"] || "1111111111111111"}.reverse if user.type == "User"
-      the_repos.reject { |r| !r.has_issues }.sort_by{|r| r["pushed_at"] || "1111111111111111"}.reverse
+      (the_repos || []).reject { |r| !r.has_issues }.sort_by{|r| r["pushed_at"] || "1111111111111111"}.reverse
     end
 
 
