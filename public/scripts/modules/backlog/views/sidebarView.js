9c68d9d4c4ef8cfb9333285523c02d318aa4aace
Ryan Rauh <rauh.ryan@gmail.com>
2013-08-21 19:02:40 -0500

    fixes the user filtering #220

--- public/scripts/modules/backlog/views/sidebarView.js
+++ public/scripts/modules/backlog/views/sidebarView.js
@@ -9,33 +9,35 @@ define(["../../common/events/postal","./filterView"], function (postal, filterVi
     },
     render: function () {
       var $this = $(this.el),
-          login = this.login;
+      login = this.login;
 
-      var assignedToMe = new filterView({color: "#0069D6", name:"Assigned to me" , condition:function(issue) { return issue.assignee && issue.assignee.login === login; } }).render();
-      var assignedToOthers = new filterView({color: "#0069D6", name:"Assigned to others" , condition:function(issue) { return issue.assignee && issue.assignee.login !== login; } }).render();
+      if (login) {
+        var assignedToMe = new filterView({color: "#0069D6", name:"Assigned to me" , condition:function(issue) { return issue.assignee && issue.assignee.login === login; } }).render();
+        var assignedToOthers = new filterView({color: "#0069D6", name:"Assigned to others" , condition:function(issue) { return issue.assignee && issue.assignee.login !== login; } }).render();
+      }
       var unassigned = new filterView({color: "#0069D6", name:"Unassigned issues" , condition:function(issue) { return !issue.assignee; } }).render();
 
-      var userFilterViews = $([assignedToMe.el,assignedToOthers.el,unassigned.el]);
+      var userFilterViews = login ? $([assignedToMe.el,assignedToOthers.el,unassigned.el]) : $(unassigned.el);
 
       $this.append(userFilterViews);
 
       userFilterViews
-          .click(function(ev) {
-           ev.preventDefault();
-           var $this = $(this),
-               $clicked = $this.data("filter");
-           var othersActive = _(userFilterViews).filter(function(v) {
-             var data = $(v).data("filter");
-                 return $clicked.cid !== data.cid && data.state !== 0;        
-           });
-           _(othersActive).each(function(v) {
-             $(v).trigger("clear");
-           });
+      .click(function(ev) {
+        ev.preventDefault();
+        var $this = $(this),
+        $clicked = $this.data("filter");
+        var othersActive = _(userFilterViews).filter(function(v) {
+          var data = $(v).data("filter");
+          return $clicked.cid !== data.cid && data.state !== 0;        
         });
+        _(othersActive).each(function(v) {
+          $(v).trigger("clear");
+        });
+      });
 
 
       var labels = _.map(this.labels, function(label) {
-          return new filterView({color: "#" + label.color, name: label.name, condition: function (issue) { return _.any(issue.labels, function(l){ return l.name.toLocaleLowerCase() === label.name.toLocaleLowerCase();})}}).render().el;
+        return new filterView({color: "#" + label.color, name: label.name, condition: function (issue) { return _.any(issue.labels, function(l){ return l.name.toLocaleLowerCase() === label.name.toLocaleLowerCase();})}}).render().el;
       });                                                                                                                                                                                                             
       $this.append("<h5>Labels</h5>");
       $this.append(labels);
