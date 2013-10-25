c7ae22182dcd3db59e7b3e538e591b5c2acedb5a
brianp <brian.o.pearce@gmail.com>
2013-10-25 01:50:40 -0700

    Assign the env variables outside of the conditional as they are now a requirement and needed in either case.

--- lib/base.rb
+++ lib/base.rb
@@ -49,15 +49,18 @@ class HuboardApplication < Sinatra::Base
     set :socket_secret, ENV["SOCKET_SECRET"]
 
     set :cache_config, {
-      servers: ENV["CACHE_SERVERS"] = ENV["MEMCACHIER_SERVERS"],
-      username: ENV["CACHE_USERNAME"] = ENV["MEMCACHIER_USERNAME"],
-      password: ENV["CACHE_PASSWORD"] = ENV["MEMCACHIER_PASSWORD"]
+      servers: ENV["MEMCACHIER_SERVERS"],
+      username: ENV["MEMCACHIER_USERNAME"],
+      password: ENV["MEMCACHIER_PASSWORD"]
     }
 
   else
     raise "Configuration information not found: you need to provide a .settings file or ENV variables"
   end
 
+  ENV["CACHE_SERVERS"]  = settings.cache_config['servers']
+  ENV["CACHE_USERNAME"] = settings.cache_config['username']
+  ENV["CACHE_PASSWORD"] = settings.cache_config['password']
 
   helpers Huboard::Common::Helpers
   helpers Sinatra::Partials
