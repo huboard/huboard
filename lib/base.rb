47377f69e497e2953df7812d8499d0e697f9599f
Ryan Rauh <rauh.ryan@gmail.com>
2014-04-03 17:56:15 -0500

    move sinatra partials

--- lib/base.rb
+++ lib/base.rb
@@ -3,31 +3,11 @@ require 'sinatra/content_for'
 require "hashie" 
 require_relative "auth/github"
 require "sinatra/asset_pipeline"
+require_relative "extensions/partials"
 
 path = File.expand_path "../jobs/*.rb", __FILE__
 Dir[path].each { |job| require(job) }
 
-# stolen from http://github.com/cschneid/irclogger/blob/master/lib/partials.rb
-#   and made a lot more robust by me
-# this implementation uses erb by default. if you want to use any other template mechanism
-#   then replace `erb` on line 13 and line 17 with `haml` or whatever
-module Sinatra::Partials
-  def partial(template, *args)
-    template_array = template.to_s.split('/')
-    template = template_array[0..-2].join('/') + "/_#{template_array[-1]}"
-    options = args.last.is_a?(Hash) ? args.pop : {}
-    options.merge!(:layout => false)
-    locals = options[:locals] || {}
-    if collection = options.delete(:collection) then
-      collection.inject([]) do |buffer, member|
-        buffer << slim(:"#{template}", options.merge(:layout =>
-        false, :locals => {template_array[-1].to_sym => member}.merge(locals)))
-      end.join("\n")
-    else
-      slim(:"#{template}", options)
-    end
-  end
-end
 
 class HuboardApplication < Sinatra::Base
 
