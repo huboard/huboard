6587fa0cd73af976231a9e08e59d03fee1618bcf
Ryan Rauh <rauh.ryan@gmail.com>
2014-04-20 22:51:59 -0500

    removes the delinquent check

--- lib/environments/production/before_filters.rb
+++ lib/environments/production/before_filters.rb
@@ -18,7 +18,7 @@ class Huboard
           customer = couch.customers.findPlanById user.id
           session[:github_login] = user.login
           session[:redirect_to] = user.login == gh.user.login ? "/settings/profile" : "/settings/profile/#/#{user.login}"
-          halt([401, "Access denied"]) if !customer.rows.any? || customer.rows.first.value.stripe.customer.delinquent
+          halt([401, "Access denied"]) if !customer.rows.any? #|| customer.rows.first.value.stripe.customer.delinquent
         end
 
       else
