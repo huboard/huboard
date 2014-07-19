fd1afe789bad6359c8353fef2b3dfb60ee395a56
Ryan Rauh <rauh.ryan@gmail.com>
2014-07-19 12:27:24 -0500

    moving middleware

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -6,19 +6,3 @@ module HuBoard
   end
 end
 
-configure :production, :staging do 
-  require "newrelic_rpm"
-  use Rack::SSL
-
-  require 'raygun4ruby'
-  raygun_api_key = ENV["RAYGUN_APIKEY"]
-
-  Raygun.setup do |config|
-    config.api_key = raygun_api_key
-    config.silence_reporting = !raygun_api_key
-  end
-
-  use Raygun::Middleware::RackExceptionInterceptor
-end
-
-
--- config/initializers/staging.rb
+++ config/initializers/staging.rb
@@ -28,3 +28,20 @@ module HuBoard
     end
   end
 end
+
+configure :production, :staging do 
+  require "newrelic_rpm"
+  use Rack::SSL
+
+  require 'raygun4ruby'
+  raygun_api_key = ENV["RAYGUN_APIKEY"]
+
+  Raygun.setup do |config|
+    config.api_key = raygun_api_key
+    config.silence_reporting = !raygun_api_key
+  end
+
+  use Raygun::Middleware::RackExceptionInterceptor
+end
+
+
