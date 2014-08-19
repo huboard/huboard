6f108c779c3763d305837e7683b1039f6564169b
Ryan Rauh <rauh.ryan@gmail.com>
2014-08-19 16:11:05 -0500

    Fully moves the integrations to the new UI

--- app/assets/javascripts/board/config/routes.js
+++ app/assets/javascripts/board/config/routes.js
@@ -3,19 +3,16 @@ var App = require('./app');
 App.Router.map(function() {
    this.resource("index",{path: "/"},function(){
     this.resource("index.issue",{path:"/issues/:issue_id"});
-    this.resource('index.integrations', {path:"/integrations"});
    })
 
    this.resource("milestones", function(){
     this.resource("milestones.issue",{path:"/issues/:issue_id"});
-    this.route('integrations');
    })
 
    this.resource("settings", function(){
 
     this.resource('settings.integrations', {path: '/integrations'}, function(){
-      this.route('new');
-                 
+      this.route('new', {path: '/new/:name'});
     });
 
    });
