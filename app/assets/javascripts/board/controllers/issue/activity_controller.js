954feb64a40e1ec0027bd8d2be99a4e7e73aeba1
Ryan Rauh <rauh.ryan@gmail.com>
2014-10-10 18:34:13 -0500

    - #402 basic UI elements to allow editing comments

--- app/assets/javascripts/board/controllers/issue/activity_controller.js
+++ app/assets/javascripts/board/controllers/issue/activity_controller.js
@@ -1,5 +1,25 @@
-var IssueActivityController = Ember.ObjectController.extend({
+var BufferedController = require("../buffered_controller");
 
+
+var IssueActivityController = BufferedController.extend({
+  needs: ["issue"],
+  mentions: Ember.computed.alias("controllers.issue.mentions"),
+  isEditing: false,
+  actions: {
+    edit: function(){
+      this.set("isEditing", true);
+    },
+    save: function() {
+      this.get('bufferedContent').applyBufferedChanges();
+      this.set("isEditing", false);
+      // do stuff
+    },
+
+    cancel: function() {
+      this.get('bufferedContent').discardBufferedChanges();
+      this.set("isEditing", false);
+    }
+  }
 })
 
 module.exports = IssueActivityController;
