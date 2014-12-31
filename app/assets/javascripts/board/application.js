a3fae112c3539b89950bf7597f5a7852e7372ce3
Richard Frank <richardfrank@me.com>
2014-12-31 09:55:45 -0700

    Dissallows dismissal of the login modal on session loss

--- app/assets/javascripts/board/application.js
+++ app/assets/javascripts/board/application.js
@@ -77284,6 +77284,22 @@ module.exports = LoadingView;
 },{"../../spin":106}],98:[function(require,module,exports){
 var ModalView = require("./modal_view")
 var LoginView = ModalView.extend({
+  didInsertElement: function() {
+    App.animateModalOpen();
+
+    this.$(".fullscreen-body").on('click.modal', function(event){
+       if(!$(event.target).parents(".hb-selector-component").length) {
+        this.$(".open")
+          .not($(event.target).parents(".hb-selector-component"))
+          .removeClass("open")
+       }
+       if($(event.target).is("[data-ember-action],[data-toggle]")){return;}
+       if($(event.target).parents("[data-ember-action],[data-toggle]").length){return;}
+       event.stopPropagation();    
+    }.bind(this))
+
+    this.$(':input:not(.close):not([type="checkbox"])').first().focus();
+  },
 });
 
 module.exports = LoginView;
