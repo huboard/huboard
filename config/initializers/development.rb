953bc1d3e9bb10f8aaa9a4d66e912517fea7af2a
Richard Frank <richardfrank@me.com>
2015-01-28 13:38:02 -0700

    Refactors development initializer

--- config/initializers/development.rb
+++ config/initializers/development.rb
@@ -5,39 +5,32 @@ module HuBoard
       before "/:user/:repo/?*" do 
 
         return if RESERVED_URLS.include? params[:user]
+        repo = gh.repos params[:user], params[:repo]
+        raise Sinatra::NotFound if repo['message'] == "Not Found"
 
-        if authenticated? :private
-          repo = gh.repos params[:user], params[:repo]
+        if authenticated?(:private) && repo["private"]
+          private_repo = UseCase::PrivateRepo.new(gh, couch)
 
-          raise Sinatra::NotFound if repo['message'] == "Not Found"
+          private_repo.run(params).match do
+            success do
+              session[:forward_to] = "#{params[:user]}/#{params[:repo]}"
+              redirect "/settings/#{params[:user]}/trial" if params[:trial_available]
 
-          if repo['private'] 
-            private_repo = UseCase::PrivateRepo.new(gh, couch)
-            private_repo.run(params).match do
-              success do
-                session[:forward_to] = "#{params[:user]}/#{params[:repo]}"
-                redirect "/settings/#{params[:user]}/trial" if params[:trial_available]
+              user = gh.users(params[:user])
+              session[:github_login] = user['login']
+              session[:upgrade_url] = user['login'] == gh.user['login'] ? "/settings/profile" : "/settings/profile/#/#{user['login']}"
+            end
 
-                user = gh.users(params[:user])
-                session[:github_login] = user['login']
-                session[:upgrade_url] = user['login'] == gh.user['login'] ? "/settings/profile" : "/settings/profile/#/#{user['login']}"
-              end
+            failure :pass_through do
+              return;
+            end
 
-              failure :pass_through do
-                return;
-              end
-
-              failure :unauthorized do
-                throw(:warden) 
-              end
+            failure :unauthorized do
+              throw(:warden) 
             end
           end
-        else
-          repo = gh.repos params[:user], params[:repo]
-          raise Sinatra::NotFound if repo['message'] == "Not Found"
         end
       end
-
     end
   end
 end
