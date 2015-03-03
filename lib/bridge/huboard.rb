c0fe80b21bb75ec95ffb2f492a392f2e98aecf96
Ryan Rauh <rauh.ryan@gmail.com>
2015-03-03 16:42:44 -0600

    fixes params key lookup

--- lib/bridge/huboard.rb
+++ lib/bridge/huboard.rb
@@ -50,7 +50,7 @@ class Huboard
                                 ]
           conn.use ClientId, params unless token || access_token
           conn.use Mimetype
-          # conn.response :logger
+          conn.response :logger
           # disable cache because github api is broken
           conn.use Caching
 
