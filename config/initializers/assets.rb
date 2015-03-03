e67811b562bd73fc96f29c9e3d419c835347ed40
Ryan Rauh <rauh.ryan@gmail.com>
2015-03-03 12:04:18 -0600

    trying to precompile assets on heroku

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,14 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( flex_layout.css marketing.js marketing/main.css vendor/jquery.js bootstrap.css bootstrap.js board/application.js vendor/jquery-ui.js )
+
+precompile_whitelist = %w(
+  .html .erb .haml
+  .png  .jpg .gif .jpeg .ico
+  .eot  .otf .svc .woff .ttf
+  .svg
+)
+Rails.application.config.assets.precompile.shift
+Rails.application.config.assets.precompile.unshift -> (path) {
+  (extension = File.extname(path)).present? and extension.in?(precompile_whitelist)
+}
