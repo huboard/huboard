d51f8bd04a95193176ae39f591b2068d1c3c93d2
Ryan Rauh <rauh.ryan@gmail.com>
2015-03-08 15:02:18 -0500

    ensures that the uber dictionary is only called once

--- test/lib/uber_dictionary_test.rb
+++ test/lib/uber_dictionary_test.rb
@@ -1,17 +1,27 @@
 require 'test_helper'
 
 class UberDictionaryTest < ActiveSupport::TestCase
-  def setup
-  end
-  def teardown
-  end
   test "should call on missing lambda once" do
-    dictionary = UberDictionary.new ->(key) { key + "_foo" }
-    
+    times = 0
+    dictionary = UberDictionary.new ->(key) { times+=1; key + "_foo" }
+
     assert dictionary['a'] == "a_foo"
+    assert_equal times, 1
     # test it again, the value should be cached now
     assert dictionary['a'] == "a_foo"
+    assert_equal times, 1
   end
+
+  test 'should consider hashes as a valid key' do
+    times = 0
+    dictionary = UberDictionary.new ->(key) { times+=1; {} }
+
+    dictionary[{a: :b}]
+    assert_equal times, 1
+    dictionary[{a: :b}]
+    assert_equal times, 1
+  end
+
   test "should call on addition" do
     on_addition = false
     dictionary = UberDictionary.new ->(key) { key + "_foo" }
