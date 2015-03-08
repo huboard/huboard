7e2458efacfc11bfd5d811edc6bf2dfea940fbba
Ryan Rauh <rauh.ryan@gmail.com>
2015-03-08 15:01:57 -0500

    adds some tests around the job resolver fallback

--- test/lib/job_resolver_test.rb
+++ test/lib/job_resolver_test.rb
@@ -1,14 +1,18 @@
 require 'test_helper'
 
 class TestExistsJob; end
+class TestJob; end
 
 class JobResolverTest < ActiveSupport::TestCase
   test "should return a no op proc" do
-    assert JobResolver.find_job(controller: "test", action: "foo") == JobResolver::Noop 
+    assert JobResolver.find_job(controller: "herp", action: "derp") == JobResolver::Noop 
   end
 
   test "should resolve a test class" do
     assert JobResolver.find_job(controller: "test", action: "exists") == TestExistsJob
   end
-  
+
+  test "should resolve the generic fallback class" do
+    assert JobResolver.find_job(controller: "test", action: "not_exists") == TestJob
+  end
 end
