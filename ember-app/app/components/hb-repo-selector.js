b57d728bcfc17b63ef1a9dfac9491b7ca2af0ea6
Richard Frank <richardfrank@me.com>
2015-05-14 10:34:54 -0600

    Begins the component and template, adds basic test

---
+++ ember-app/app/components/hb-repo-selector.js
@@ -0,0 +1,55 @@
+import Ember from 'ember';
+
+var HbRepoSelectorComponent = Ember.Component.extend({
+  classNameBindings: [":hb-selector-component", ":dropdown"],
+  isOpen: function(){
+    return false;
+  }.property(),
+  actions: {
+    toggleSelector: function(){
+      this.set("isOpen", !!!this.$().is(".open"));
+      if(this.get("isOpen")) {
+        Ember.$(".open").removeClass("open");
+        this.$().addClass("open");
+        this.$(':input:not(.close):not([type="checkbox"])').first().focus();
+        this.set("filterRepos", "");
+
+      } else {
+        this.$().removeClass("open");
+      }
+    },
+    assignTo: function(milestone) {
+      this.set("selected", milestone);
+      this.sendAction("assign", milestone);
+      this.$().removeClass("open");
+      this.set("isOpen", false);
+    }
+  },
+
+  didInsertElement: function() {
+    Ember.$('body').on('keyup.flyout', function(event) {
+      if (event.keyCode === 27){ this.set("isOpen", false); }
+    }.bind(this));
+
+    this.$(".hb-flyout").on('click.flyout', function(event){
+      if(Ember.$(event.target).is("[data-ember-action],[data-toggle]")){return;}
+      if(Ember.$(event.target).parents("[data-ember-action],[data-toggle]").length){return;}
+      event.preventDefault();
+      event.stopPropagation();  
+      this.set("isOpen", false);
+    }.bind(this));
+
+    this.$(".close").on('click.flyout', function(event){
+      if(Ember.$(event.target).is("[data-ember-action],[data-toggle]")){return;}
+      if(Ember.$(event.target).parents("[data-ember-action],[data-toggle]").length){return;}
+      this.set("isOpen", false);
+    }.bind(this));
+  },
+  willDestroyElement: function() {
+    Ember.$('body').off('keyup.flyout');
+    this.$(".hb-flyout,.close").off("click.modal");
+  }
+
+});
+
+export default HbRepoSelectorComponent;
