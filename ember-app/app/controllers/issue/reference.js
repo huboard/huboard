649962223f5c9557277ff08e004c71ac3920428f
Richard Frank <richardfrank@me.com>
2015-05-26 14:51:57 -0600

    Fixes broken model references on issue fullscreen

--- ember-app/app/controllers/issue/reference.js
+++ ember-app/app/controllers/issue/reference.js
@@ -5,7 +5,7 @@ var IssueReferenceController = Ember.Controller.extend({
   needs: ["issue"],
 
   fetchCommit: function(commit){
-    var repo = this.get("controllers.issue.repo.full_name");
+    var repo = this.get("controllers.issue.model.repo.full_name");
     return ajax("/api/" + repo + "/commit/" + commit)
       .then(function(response){
         return response;
