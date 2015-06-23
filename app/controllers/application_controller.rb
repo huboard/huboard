97fb1b538d13ea2be12bfd1aaa930dd425dd9b60
Richard Frank <richardfrank@me.com>
2015-06-23 08:53:54 -0600

    Removes uneeded spaces

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   rescue_from ActionController::InvalidAuthenticityToken, :with => :csrf_failed
-
   rescue_from Ghee::Unauthorized, :with => :ghee_unauthorized
-
 
   include ApplicationHelper
 
