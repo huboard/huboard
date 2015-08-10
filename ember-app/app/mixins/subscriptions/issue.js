1068099b6fb0f4f37cad73136c4177df07e504c4
Richard Frank <richardfrank@me.com>
2015-08-10 11:26:55 -0600

    Implements subscriber for issue_commented

--- ember-app/app/mixins/subscriptions/issue.js
+++ ember-app/app/mixins/subscriptions/issue.js
@@ -4,29 +4,38 @@ var IssueSubscriptionMixin = Ember.Mixin.create({
   hbsubscriptions: {
     channel: "{model.repo.full_name}",
     "issues.{model.number}.issue_closed": "closed",
-    "issues.{model.number}.issue_reopened": "opened"
+    "issues.{model.number}.issue_reopened": "opened",
+    "issues.{model.number}.issue_commented": "commented"
   },
   hbsubscribers: {
+    commented: function(message){
+      var activities = this.get("sortedActivities");
+      var unique = this.hbsubscribers._activityUnique;
+      if(!activities.length || unique(activities, message.comment.updated_at)){
+        var comment = this.hbsubscribers._comment(message);
+        this.get("model.activities.comments").pushObject(comment);
+      }
+    },
     closed: function(message){
       var activities = this.get("sortedActivities");
-      var unique = this._eventHandlers._activityUnique;
-      if(!activities.length || unique(activities, message)){
-        var activity = this._eventHandlers._activity(message, "closed");
-        activities.pushObject(activity);
+      var unique = this.hbsubscribers._activityUnique;
+      if(!activities.length || unique(activities, message.issue.updated_at)){
+        var activity = this.hbsubscribers._activity(message, "closed");
+        this.get("model.activities.events").pushObject(activity);
       }
     },
     opened: function(message){
       var activities = this.get("sortedActivities");
-      var unique = this._eventHandlers._activityUnique;
-      if(!activities.length || unique(activities, message)){
-        var activity = this._eventHandlers._activity(message, "reopened");
-        activities.pushObject(activity);
+      var unique = this.hbsubscribers._activityUnique;
+      if(!activities.length || unique(activities, message.issue.updated_at)){
+        var activity = this.hbsubscribers._activity(message, "reopened");
+        this.get("model.activities.events").pushObject(activity);
       }
     },
     //This method only works for client-built activities
-    _activityUnique: function(activities, message){
+    _activityUnique: function(activities, timestamp){
       var activity = activities.get("lastObject");
-      return activity.created_at !== message.issue.updated_at;
+      return activity.created_at !== timestamp;
     },
     _activity: function(message, event){
       var issue = message.issue;
@@ -36,6 +45,18 @@ var IssueSubscriptionMixin = Ember.Mixin.create({
         event: event,
         created_at: issue.updated_at,
         actor: issue.user
+      }
+    },
+    _comment: function(message){
+      var issue = message.issue;
+      return {
+        body: message.comment.body,
+        body_html: message.comment.body_html,
+        body_text: message.comment.body_text,
+        html_url: message.comment.html_url,
+        issue_url: message.comment.issue_url,
+        created_at: issue.updated_at,
+        user: issue.user
       }
     }
   }
